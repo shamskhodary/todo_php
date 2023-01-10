6af45ee6a6c33c9d344fa37ecc53389a667fd94f
shamskhodary <shams.khodary@gmail.com>
2023-01-10 20:02:37 +0200

    created executeSQL function to run the queries, and close connection fn relates #3

--- api/dbConnection.php
+++ api/dbConnection.php
@@ -18,9 +18,29 @@ class DBConnection
     echo "Connected successfully \n";
   }
 
+
   public function getConnection()
   {
     return $this->mysqli;
+  }
+
+  public function executeSQL()
+  {
+    try {
+      $sql = file_get_contents('db.sql');
+
+      if ($this->mysqli->multi_query($sql)) {
+
+        echo "SQL file executed successfully";
+      }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+  }
+
+  public function close()
+  {
+    return $this->mysqli->close();
   }
 }
 
